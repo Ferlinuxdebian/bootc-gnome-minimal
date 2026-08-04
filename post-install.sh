52680a4073d8bc5fc348caa9df0432055e517004
Fernando Fedora <fernandodebianbr@gmail.com>
2026-08-04 11:42:16 -0300

    O script de instalar os flatpaks pós install foi melhorado

--- post-install.sh
+++ post-install.sh
@@ -1,52 +1,74 @@
 #!/bin/bash
 set -e
 
+MARKER="/var/lib/flatpak-bootstrap.done"
+
+if [ -f "$MARKER" ]; then
+    exit 0
+fi
+
 flatpak remote-delete fedora
 sleep 2
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 sleep 2
-flatpak install -y flathub \
-app.drey.Dialect \
-be.alexandervanhee.gradia \
-ca.desrt.dconf-editor \
-com.brave.Browser \
-com.dec05eba.gpu_screen_recorder \
-com.github.finefindus.eyedropper \
-com.github.tchx84.Flatseal \
-com.mattjakeman.ExtensionManager \
-com.obsproject.Studio \
-com.protonvpn.www \
-com.spotify.Client \
-fr.handbrake.ghb \
-im.riot.Riot \
-io.github.celluloid_player.Celluloid \
-io.github.fabrialberio.pinapp \
-io.github.flattool.Ignition \
-io.github.flattool.Warehouse \
-io.github.getnf.embellish \
-io.github.josephmawa.TextCompare \
-io.github.onionware_github.onionmedia \
-io.github.thetumultuousunicornofdarkness.cpu-x \
-io.gitlab.news_flash.NewsFlash \
-io.missioncenter.MissionCenter \
-io.mpv.Mpv \
-io.typora.Typora \
-nl.hjdskes.gcolor3 \
-org.altlinux.Tuner \
-org.deluge_torrent.deluge \
-org.gimp.GIMP \
-org.gnome.Calculator \
-org.gnome.Evince \
-org.gnome.Loupe \
-org.gnome.font-viewer \
-org.gnome.meld \
-org.libreoffice.LibreOffice \
-org.mozilla.Thunderbird \
-org.mozilla.firefox \
-org.shotcut.Shotcut \
-org.telegram.desktop \
-org.videolan.VLC \
-page.codeberg.libre_menu_editor.LibreMenuEditor \
-page.tesk.Refine \
-xyz.tytanium.DoorKnocker
 
+apps=(
+app.drey.Dialect
+be.alexandervanhee.gradia
+ca.desrt.dconf-editor
+com.brave.Browser
+com.dec05eba.gpu_screen_recorder
+com.github.finefindus.eyedropper
+com.github.tchx84.Flatseal
+com.mattjakeman.ExtensionManager
+com.obsproject.Studio
+com.protonvpn.www
+com.spotify.Client
+fr.handbrake.ghb
+im.riot.Riot
+io.github.fabrialberio.pinapp
+io.github.flattool.Ignition
+io.github.flattool.Warehouse
+io.gitlab.news_flash.NewsFlash
+io.mpv.Mpv
+io.typora.Typora
+net.nokyan.Resources
+org.altlinux.Tuner
+org.deluge_torrent.deluge
+org.gimp.GIMP
+org.gnome.Calculator
+org.gnome.Evince
+org.gnome.Loupe
+org.gnome.Showtime
+org.gnome.meld
+org.libreoffice.LibreOffice
+org.localsend.localsend_app
+org.mozilla.firefox
+org.shotcut.Shotcut
+org.telegram.desktop
+org.upscayl.Upscayl
+org.videolan.VLC
+page.codeberg.libre_menu_editor.LibreMenuEditor
+page.tesk.Refine
+xyz.tytanium.DoorKnocker
+)
+
+installed=$(flatpak list --app --columns=application)
+
+missing=()
+
+for app in "${apps[@]}"; do
+    if ! grep -qx "$app" <<< "$installed"; then
+        missing+=("$app")
+    fi
+done
+
+if [ "${#missing[@]}" -eq 0 ]; then
+    touch "$MARKER"
+    exit 0
+fi
+
+echo "Instalando Flatpaks ausentes..."
+flatpak install -y flathub "${missing[@]}"
+
+touch "$MARKER"
